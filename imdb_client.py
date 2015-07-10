5f902368b11509b82fcf11f1d5bd9599db344816
Kiran Balakrishnan <kiran.balakrishnan@rapidvaluesolutions.com>
2015-07-10 19:25:51 +0530

    added imd client to get info from imdbpy

---
+++ imdb_client.py
@@ -0,0 +1,36 @@
+"""
+Module used to get ratings, director, plot and genere of a movie
+using imdbpy
+"""
+
+import imdb
+i = imdb.IMDb()
+
+def search_movie(movie_name, movie_year=None):
+    movies = i.search_movie(movie_name)
+    selected_movie = None
+    if movies:
+        if movie_year:
+            for movie in movies:
+                if movie_year == movie.data.get('year'):
+                    selected_movie = movie
+                    break
+        else:
+            selected_movie = movies[0]
+    return selected_movie
+
+def get_movie_details(movie):
+    if movie:
+        movie = i.get_movie(movie.movieID)
+        d = None
+        if movie:
+            data = movie.__dict__['data']
+            d = {'title': data.get('title'),
+                 'ratings': data.get('rating'), 
+                 'plot': data.get('plot'),
+                  'director':data.get('director')[0].get('name')}
+        return d
+
+def imdb_details(movie_name, movie_year=None):
+    movie = search_movie(movie_name, movie_year)
+    return get_movie_details(movie)
